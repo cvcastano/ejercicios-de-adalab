935d609e3735a43bbee71b35380943a5b3b8bff6
cvcastano <carocastanocastillo@gmail.com>
2021-01-22 07:28:25 +0100

    start lesson 4

---
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-01-times-for-function/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function timesFor(a, b) {
+    const result = a * b;
+    alert (result);
+}
+// const timesForResult = timesFor(7, 3);
+// const timesForResult = timesFor(9, 6);
+const timesForResult = timesFor(5, 8);
+
+
