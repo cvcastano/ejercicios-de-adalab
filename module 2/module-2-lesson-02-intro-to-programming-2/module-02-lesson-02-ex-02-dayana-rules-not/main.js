a59400af698d4cd1f01c24fc04a8ea7d5ef98d9a
cvcastano <carocastanocastillo@gmail.com>
2021-01-22 20:24:13 +0100

    fix ex.02

--- module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-02-dayana-rules-not/main.js
+++ module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-02-dayana-rules-not/main.js
@@ -1,6 +1,9 @@
 'use strict';
 const superWave = document.querySelector('.super-wave');
 
-// Tenemos que crear una clase nueva para destacar el texto OLA! y añadirla con JavaScript al elemento con clase super-wave. A por ello!
+// Tenemos que crear una clase nueva para destacar 
+// el texto OLA! y añadirla con JavaScript al elemento 
+//con clase super-wave. A por ello!
 // Prueba a loguear la constante superWave.
-SpanElement.classList.add('super-wave');
+
+superWave.classList.add('highlight'); 
