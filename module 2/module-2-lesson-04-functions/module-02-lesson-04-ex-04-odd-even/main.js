59dbacb9f8bb25155a5564e70d4dab26f1d30fcc
cvcastano <carocastanocastillo@gmail.com>
2021-01-22 21:36:35 +0100

    add exe 04

---
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-04-odd-even/main.js
@@ -0,0 +1,15 @@
+'use strict';
+function oddEven(num) {
+if (num % 2 === 0) {
+    return true;
+}
+else {
+    return false;
+}
+}
+alert (oddEven(8));
+alert (oddEven(9));
+alert (oddEven (456));
+
+
+// slash (/) divide, el porciento (%) halla el resto de la división
