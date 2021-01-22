41ff9a22e787200a74c5721772265295251344bf
cvcastano <carocastanocastillo@gmail.com>
2021-01-22 21:04:22 +0100

    add ex.03j

--- module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-03-ticket-with-vat/main.js
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-03-ticket-with-vat/main.js
@@ -1,16 +1,13 @@
 'use strict';
 
+function iva(a) {
+    return `Precio sin IVA: ${a}, IVA: ${a*0.21} y Total: ${a+ a*0.21}`; 
+}
+iva(10);
 
-
-function calculation (a, b) {
-    const importe = a; 
-    const result = a * b; 
+console.log (iva(10));
     
-    return result;
-    }
-    
-    const sumResult = calculation(10, 0.21);
-    
+// "Precio sin IVA: 10, IVA: 2,1 y Total: 12,1"
 
-
-alert ('Precio sin IVA: ' `${importe} ', IVA:  ´${sumResult}´ ' Total: ' ´${importe * sumResult}´)
+    
+   
