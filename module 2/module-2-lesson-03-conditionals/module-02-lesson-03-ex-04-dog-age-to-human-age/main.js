0bc3b363502a57397173eb97700858d0ba720d1e
cvcastano <carocastanocastillo@gmail.com>
2021-01-23 21:18:41 +0100

    add ex. 04

---
+++ module 2/module-2-lesson-03-conditionals/module-02-lesson-03-ex-04-dog-age-to-human-age/main.js
@@ -0,0 +1,9 @@
+'use strict';
+const dogAge = (14);
+if (dogAge === 1) {
+    console.log ('the dog is 15 human years old');
+} else if (dogAge === 2) {
+    console.log (`the dog is ${15 + 9} human years old`);
+} else {
+    console.log (`the dog is ${15 + 9 + 5*(dogAge - 2)} human years old`);
+}
