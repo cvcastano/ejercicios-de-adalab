362005081db9f92f1539866767aef61fe6926382
cvcastano <carocastanocastillo@gmail.com>
2021-01-23 17:58:41 +0100

    add ex. 09

--- module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-09-number-of-letters-in-your-name/main.js
+++ module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-09-number-of-letters-in-your-name/main.js
@@ -1,1 +1,6 @@
 'use strict';
+const name = 'Carolina'; 
+const surname = 'Castaño'; 
+const surname2 = 'Castillo';
+const charactersSum = name + surname + surname2;
+alert (`Mi nombre es ${name} ${surname} ${surname2}, y está compuesto por ${charactersSum.length} caracteres`);
