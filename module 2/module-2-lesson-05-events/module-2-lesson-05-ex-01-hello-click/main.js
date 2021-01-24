481af860b0111da2c1e764fad1f2a58b38edbc86
cvcastano <carocastanocastillo@gmail.com>
2021-01-24 16:27:40 +0100

    add ex 1

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-01-hello-click/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-01-hello-click/main.js
@@ -1,1 +1,10 @@
 'use strict';
+// elementos de HTML
+const button = document.querySelector ('button');
+const title = document.querySelector ('h1');
+// handler
+function changeTitle () {
+    title.innerHTML = "Mi primer click, ¡ole yo y la mujer que me parió";
+}
+// listener sobre el botón, con tipo de evento y handler
+button.addEventListener('click',changeTitle);
