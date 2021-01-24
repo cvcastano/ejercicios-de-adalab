d9d286d24e574faf7242156caf5f75ecef054b98
cvcastano <carocastanocastillo@gmail.com>
2021-01-24 17:27:48 +0100

    add ex. 03

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-03-give-me-ipsum/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-03-give-me-ipsum/main.js
@@ -1,1 +1,19 @@
 'use strict';
+// elemento de HTML
+const paragraph = document.querySelector('p');
+
+// handler
+function addLorem() {
+ paragraph.innerHTML = paragraph.innerHTML + paragraph.innerHTML ;
+}
+
+// listener sobre el elemento, con tipo de evento y handler
+paragraph.addEventListener('mouseenter', addLorem);
+
+
+
+
+
+// Crear una página HTML con un párrafo con lorem ipsum. 
+// Al poner el ratón sobre el párrafo, vamos a añadir un 
+// nuevo párrafo a la página con lorem ipsum.
