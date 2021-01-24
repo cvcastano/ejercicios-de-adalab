6a4beccc078e3a1155bc84017c3ce309adb12bf5
cvcastano <carocastanocastillo@gmail.com>
2021-01-24 18:33:19 +0100

    add ex 04

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-04-colored-scroll/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-04-colored-scroll/main.js
@@ -1,1 +1,27 @@
 'use strict';
+const div = document.querySelector('.color'); 
+
+
+function changeColor() {
+
+    if (window.scrollY < 250) {
+        div.classList.add('color');
+        div.classList.remove('color2');
+    } else {
+        div.classList.add('color2');
+        div.classList.remove('color');
+    }
+
+}
+
+window.addEventListener('scroll', changeColor);
+
+
+
+
+
+// Cambiar el color de fondo de la página cuando se haga scroll. Para ello tenemos que:
+
+// Escuchar el evento scroll sobre la ventana window.
+// Cuando la posición del scroll vertical supere 250 píxeles poner un color de fondo, cuando sea inferior a 250 píxeles poner otro.
+// Nota: window.scrollY nos devuelve la posición del scroll vertical.
