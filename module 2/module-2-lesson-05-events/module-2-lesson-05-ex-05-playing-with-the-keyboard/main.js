47cf15ac809694fab73f714779ba18a4620e3900
cvcastano <carocastanocastillo@gmail.com>
2021-01-24 19:14:21 +0100

    add ex.05

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-05-playing-with-the-keyboard/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-05-playing-with-the-keyboard/main.js
@@ -1,1 +1,27 @@
 'use strict';
+const body  = document.querySelector('body');
+
+function changeColor () {
+    if (event.keyCode === 82){
+body.classList.add ('red');
+body.classList.remove ('purple');
+
+    }
+    else if (event.keyCode === 77) {
+        body.classList.add ('purple');
+        body.classList.remove ('red');
+        
+    }
+}
+document.addEventListener('keydown', changeColor);
+
+
+
+
+
+
+// Tenemos que crear una página vacía. 
+// Al pulsar la tecla 'r' su color de fondo 
+// cambia a rojo y al pulsar la 'm' a morado. 
+// Vamos a escuchar un evento de teclado 
+// directamente sobre el elemento document.
