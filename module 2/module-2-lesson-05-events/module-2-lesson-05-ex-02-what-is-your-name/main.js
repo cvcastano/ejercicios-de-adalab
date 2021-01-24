47fd4400b27c0c3176cc4d07f1f4f7ffaa5a2f35
cvcastano <carocastanocastillo@gmail.com>
2021-01-24 17:09:26 +0100

    add ex 02

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-02-what-is-your-name/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-02-what-is-your-name/main.js
@@ -1,1 +1,12 @@
 'use strict';
+// coger los elementos de HTML
+const button = document.querySelector('button');
+const text = document.querySelector('input');
+
+
+//handler
+function changeText () {
+   alert (`Hola, ${text.value}`);  
+
+}
+button.addEventListener('click',changeText);
