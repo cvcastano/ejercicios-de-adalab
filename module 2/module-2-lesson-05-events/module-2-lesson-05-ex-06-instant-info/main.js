713814d0d59b4c5b9ca02bb746a6d1ecd52a0922
cvcastano <carocastanocastillo@gmail.com>
2021-01-25 07:09:29 +0100

    add ex.6

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-06-instant-info/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-06-instant-info/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const input = document.querySelector('input');
+const paragraph = document.querySelector('p');
+
+function pickUpValue () {
+    paragraph.innerHTML= event.currentTarget.value;
+}
+input.addEventListener('keyup', pickUpValue);
+
+
+
+// Crear una página con un input de texto y un párrafo vacío. 
+// Cada vez que la usuaria escriba una letra tenemos que 
+// recoger el valor del input al que le pusimos el listener 
+// y escribirlo en el párrafo.
+// Nota: el objetivo es hacerlo utilizando 
+// event.currentTarget.
