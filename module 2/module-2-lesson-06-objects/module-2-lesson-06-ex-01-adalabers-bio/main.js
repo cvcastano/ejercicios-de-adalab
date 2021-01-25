ce4c6abe43c86840a550987ec0d30944cd22b920
cvcastano <carocastanocastillo@gmail.com>
2021-01-25 20:51:26 +0100

    add ex. 01

--- module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-01-adalabers-bio/main.js
+++ module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-01-adalabers-bio/main.js
@@ -1,1 +1,21 @@
 'use strict';
+// Crea un nuevo objeto en JavaScript adalaber1 que nos sirva para representar (modelar) a una Adalaber. Tenemos estos datos:
+// Susana, 34 años, periodista
+
+const adalaber1 = {
+    name: 'Susana',
+    age: 34,
+    job: 'periodista'
+};
+// const sentence = document.querySelector ('h1');
+// sentence.innerHTML = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} años y soy ${adalaber1.job}.`;
+
+// Ahora hacemos lo mismo (crear el objeto adalaber2 y mostrar una frase descriptiva) con una nueva Adalaber con estos datos:
+// Rocío, 25 años, actriz
+const adalaber2 = {
+    name: 'Rocío',
+    age: 25,
+    job: 'actriz'
+};
+const sentence = document.querySelector ('h1');
+sentence.innerHTML = `Mi nombre es ${adalaber2.name}, tengo ${adalaber2.age} años y soy ${adalaber2.job}.`;
