2a58a92bb7d33949762687d6e15c4bba53a1dd99
cvcastano <carocastanocastillo@gmail.com>
2021-01-25 07:23:07 +0100

    add ex 7

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-07-another-button/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-07-another-button/main.js
@@ -1,1 +1,17 @@
 'use strict';
+const button = document.querySelector('button');
+function addClass () {
+button.classList.toggle('custom');
+}
+button.addEventListener ('click', addClass);
+
+
+// Vamos a preparar un botón y una clase de CSS. 
+// La clase tiene que cambiar alguno de los estilos del botón 
+// (por ejemplo el color de fondo), pero no se la vamos a 
+// poner inicialmente. 
+// Cada vez que la usuaria pulse el botón hay que:
+// añadir la clase si no la tiene
+// quitarla la clase si la tiene
+// Nota: para facilitar añadir y quitar clases de CSS, 
+// os recomendamos usar classList.toggle
