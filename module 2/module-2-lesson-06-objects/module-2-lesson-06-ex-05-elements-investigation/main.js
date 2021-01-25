c622a162d881585c460a60ad0cf22da924254a73
cvcastano <carocastanocastillo@gmail.com>
2021-01-25 22:25:56 +0100

    add ex 05

--- module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-05-elements-investigation/main.js
+++ module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-05-elements-investigation/main.js
@@ -1,1 +1,8 @@
 'use strict';
+// Vamos a preparar un input de tipo texto, a recogerlo desde JavaScript y a imprimirlo en la consola con console.dir para investigarlo y comentar con la compañera estas propiedades:
+// value
+// nodeName
+// required
+// Nota Cuando imprimimos un elemento en la consola con console.log vemos la etiqueta de HTML y podemos desplegarla para ver su contenido. Si queremos ver el objeto con sus propiedades y métodos tendremos que usar console.dir.
+const inputElement = document.querySelector ('input');
+console.dir(inputElement);
