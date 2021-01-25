f79e0604d43e4c2f1678c9067b76ef47708e1295
cvcastano <carocastanocastillo@gmail.com>
2021-01-25 07:58:06 +0100

    add ex. 08

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-08-more-buttons/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-08-more-buttons/main.js
@@ -1,1 +1,18 @@
 'use strict';
+'use strict';
+const button1 = document.querySelector('.button1');
+const button2 = document.querySelector('.button2');
+function addClass () {
+const clickedButton = event.currentTarget;
+clickedButton.classList.toggle('custom');
+}
+button1.addEventListener ('click', addClass);
+button2.addEventListener ('click', addClass);
+
+
+// Partiendo del ejercicio anterior vamos a añadir un nuevo botón 
+// a nuestra página. Tenemos que ponerle un listener y a reutilizar 
+// el handler que ya teníamos. Es decir, los dos botones deben tener 
+// el mismo handler.
+// Cuando la usuaria pulse un botón el cambio de clase sucederá 
+// solamente sobre el botón pulsado.
