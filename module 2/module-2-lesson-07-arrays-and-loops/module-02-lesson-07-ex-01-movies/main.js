7d250759f012a369530bc4eac5056c69a190ba0e
cvcastano <carocastanocastillo@gmail.com>
2021-01-26 21:26:57 +0100

    add ex 01

--- module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-01-movies/main.js
+++ module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-01-movies/main.js
@@ -1,1 +1,21 @@
 'use strict';
+// crear un array movies con un listado de 3 películas que te gusten
+const movies = ['pulpFiction', 'seven', 'goneWithTheWind'];
+
+// añade al array anterior otra película 
+movies[3] = 'littleMissSunshine';
+
+// modificar la película que menos te guste
+movies[2] = 'matrix';
+
+// modificar la película que menos te guste
+movies[0] = 'findingNemo';
+
+// mostrar el array completo en la consola
+console.log(movies);
+
+// encapsular todo el código que hemos creado en una función que no toma parámetros y que llamaremos workWithMovies. Ejecutamos la función para comprobar que se muestran los mensajes en la consola correspondientes.
+
+function workWithMovies () {
+
+}
