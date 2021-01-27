31d708f2d1d2415578a920e125d62c442840443a
cvcastano <carocastanocastillo@gmail.com>
2021-01-27 08:14:55 +0100

    add ex. 05

--- module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-05-average/main.js
+++ module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-05-average/main.js
@@ -1,1 +1,9 @@
 'use strict';
+const numbers = [2, 3, 5, 8, 9];
+let acc = 0;
+for (let i= 0; i < numbers.length; i++) {
+    acc += numbers[i]/numbers.length;
+    
+    
+}
+console.log(`The average is ${acc}`);
