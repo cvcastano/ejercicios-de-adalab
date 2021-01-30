9c3b5672af141e05ba9a9f1a1a735af6d32c028d
cvcastano <carocastanocastillo@gmail.com>
2021-01-30 20:35:53 +0100

    add part of ex. 04

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-04-my-to-do-list/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-04-my-to-do-list/main.js
@@ -1,1 +1,20 @@
 'use strict';
+const tasks = [
+    { name: 'Recoger setas en el campo', completed: true },
+    { name: 'Comprar pilas', completed: true },
+    { name: 'Poner una lavadora de blancos', completed: true },
+    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+        completed: false}
+];
+
+// Nuestra misión es:
+// Mostrar una frase que indique cuántas tareas hay.
+const paragraphElement = document.querySelector ('p');
+const tasksAmount = tasks.length;
+paragraphElement.innerHTML = `Number of things to do: ${tasksAmount}`;
+// Pintar todas las tareas en pantalla.
+const listElement = document.querySelector ('ul');
+listElement.innerHTML = '';
+
+// Tachar las ya realizadas.
+// Permitir marcar una tarea como 'completa' o 'incompleta'.
