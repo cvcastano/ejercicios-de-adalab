680732d0cf60fa7dc0b7bc698ab7b08bc1c6c712
cvcastano <carocastanocastillo@gmail.com>
2021-01-30 18:32:21 +0100

    add ex. 02

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let array100 = []
+
+ function get100Numbers () {
+    for (let i= 0; i<100; i++) {
+       array100.push(i+1);
+       
+    }
+return array100
+}
+ 
+function getReversed100Numbers () {
+   let reversedArray100 = get100Numbers().reverse();
+    console.log(reversedArray100);
+}
+getReversed100Numbers();
