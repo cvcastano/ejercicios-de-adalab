36b34aa79cf6b657e49c346d77d1026c87d1f49b
cvcastano <carocastanocastillo@gmail.com>
2021-01-30 11:58:02 +0100

    add folders

--- module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-05-average/main.js
+++ module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-05-average/main.js
@@ -1,9 +1,35 @@
 'use strict';
-const numbers = [2, 3, 5, 8, 9];
+//  const numbers = [0, 3, 11, 13, 33];
+//  let acc = 0;
+// for (let i= 0; i < numbers.length; i++) {
+//     acc += numbers[i];    
+// }
+// console.log(`The average is ${acc/numbers.length}`);
+
+// const numbers = [0, 3, 11, 13, 33];
+//  let acc = 0;
+// numbers [5] = 10 ;
+// for (let i= 0; i < numbers.length; i++) {
+//     acc += numbers[i]/numbers.length; 
+// }
+
+
+// const numbers = [0, 3, 11, 13, 33, 10];
+// let acc = 0;
+// function average (numbers) {
+//     for (let i= 0; i < numbers.length; i++) {
+//          acc += numbers[i]/numbers.length; 
+//         };
+//         return `La media es: ${acc}`;
+// }
+// console.log(average (numbers));
+
+const numbers = [5, 5, 5];
 let acc = 0;
-for (let i= 0; i < numbers.length; i++) {
-    acc += numbers[i]/numbers.length;
-    
-    
+function average (numbers) {
+    for (let i= 0; i < numbers.length; i++) {
+         acc += numbers[i]/numbers.length; 
+        };
+        return `La media es: ${acc}`;
 }
-console.log(`The average is ${acc}`);
+console.log(average (numbers));
