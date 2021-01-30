0258e6a9dabeb4818abf3f0229088c0ac3a7116b
cvcastano <carocastanocastillo@gmail.com>
2021-01-30 12:48:22 +0100

    add ex.01

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-01-little-numbers/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-01-little-numbers/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+ let array100 = []
+
+ function get100Numbers () {
+    for (let i= 0; i<100; i++) {
+       array100.push(i+1);
+       console.log(array100);
+    }
+
+}
+get100Numbers();
