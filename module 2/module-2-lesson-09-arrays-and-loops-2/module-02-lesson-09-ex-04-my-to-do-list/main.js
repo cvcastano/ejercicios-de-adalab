6a86c8a5029304e34dfbf8a8a66ee1e7ed64f79f
cvcastano <carocastanocastillo@gmail.com>
2021-01-31 20:33:44 +0100

    can't do it right

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-04-my-to-do-list/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-04-my-to-do-list/main.js
@@ -3,36 +3,63 @@ const tasks = [
     { name: 'Recoger setas en el campo', completed: true },
     { name: 'Comprar pilas', completed: true },
     { name: 'Poner una lavadora de blancos', completed: true },
-    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-        completed: false}
+    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript', completed: false },
+    { name: 'Conseguir hacer este ejercicio bien, a pesar de llevar todo el día con él', completed: false }
 ];
-
-// Mostrar una frase que indique cuántas tareas hay.
-const body = document.querySelector ('body');
+// Tareas totales
+const body = document.querySelector('body');
 const tasksAmount = tasks.length;
-body.innerHTML += `<p>Number of things to do: ${tasksAmount}</p>`;
-
+body.innerHTML += `<p class="outcome">Tienes ${tasksAmount} tareas.</p>`; // Mostrar una frase que indique cuántas tareas hay.
+                                                                          // y un span para luego      
 // Pintar todas las tareas en pantalla.
-body.innerHTML += '<ul></ul>';                          //crear una lista
-const listElement = document.querySelector('ul');       //seleccionar la lista
-//listElement.innerHTML += `<li>${tasks[0].name}</li>`; //pintar 1ª tarea en un <li>
-                                                        //NO HE SABIDO TACHAR SÓLO ESA TAREA
-function paintTasks () {     
-for (let i = 0; i< tasks.length; i++) {
-listElement.innerHTML += `<li>${tasks[i].name}</li>`;   //pintar todas las tareas
-let lastListItem = listElement.querySelector('li:last-child'); //crear variable del último <li>     
-                                                            //(al ir x orden, los recorrerá todos)
-console.log (lastListItem);                             //POR QUÉ NO MUESTRA EL ÚLTIMO??
-if (tasks[i].completed === true) { 
-     lastListItem.classList.add ('cross_off');           // Tachar las ya realizadas
- }
- listElement.innerHTML += '<input type="checkbox"></input>'; //add inputs a los <li>
+body.innerHTML += '<ul></ul>';                                      // crear una lista
+const listElement = document.querySelector('ul');                   // variable de la lista
+//listElement.innerHTML += `<li>${tasks[0].name}</li>`;             // pintar 1ª tarea en un <li>
+
+function paintTasks() {
+    for (let i = 0; i < tasks.length; i++) {
+
+        const eachTask = `${tasks[i].name}`;                        // variable de cada tarea
+
+        listElement.innerHTML += `<li>${eachTask}</li>`;            // pintar todas las tareas
+        let listItem = listElement.querySelector('li:last-child');  // crear variable del último <li>(al ir x orden, los recorre todos)
+
+        if (tasks[i].completed === true) {
+            listItem.classList.add('cross_off');                    // Tachar las ya realizadas
+            const checked = `<input type="checkbox" checked>`;      // variable del box con check
+            listItem.innerHTML += checked;                          // incluirlo en las tareas completadas
+        } else {
+            const unchecked = '<input type="checkbox">';            // variable del box sin check
+            listItem.innerHTML += unchecked;                        // incluirlo en las tareas por hacer
+        }
+    }
 }
+paintTasks();                                                       //ejecutar función paintTasks
+// listElement.classList.add('cross_off');                          //tachar todas las tareas
+
+// Darle dinamismo a la lista
+const eachBox = document.querySelectorAll('input');                 // variable de todos los checkbox
+
+const listItems = document.querySelectorAll('li');                  // variable con todos los <li>
+
+for (let i = 0; i < eachBox.length; i++) {
+    eachBox[i].addEventListener('click', handleStatus)              // listener del click
 }
-paintTasks ();                                          //ejecutar función paintTasks
-// listElement.classList.add('cross_off');              //tachar todas las tareas   
+function handleStatus() {                                           // función para marcar tareas como 'completas' o 'incompletas'.
+    for (let i = 0; i < eachBox.length; i++) {
+        if (eachBox[i].checked === true) {
+            tasks[i].completed = true;
+            listItems[i].classList.add('cross_off');
+        } else {
+            tasks[i].completed = false;
+            listItems[i].classList.remove('cross_off');
+        }
+    }
+}
+const tasksDone = document.querySelectorAll (':checked');           // variable de los checked boxes
+const tasksToDo = parseInt(tasksAmount) - parseInt(tasksDone.length); // número de tareas por hacer
+console.log (tasksToDo);
+let outcome = document.querySelector('.outcome');
+outcome.innerHTML = `Tienes ${tasksAmount} tareas. ${tasksDone.length} completadas y ${tasksToDo} por realizar.`
 
-
-
- 
-// Permitir marcar una tarea como 'completa' o 'incompleta'.
+handleStatus ();
