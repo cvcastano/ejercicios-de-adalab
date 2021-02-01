6d1d5ac34a0d998d991e910d37aa39a748b92582
cvcastano <carocastanocastillo@gmail.com>
2021-02-01 18:20:32 +0100

    add ex. 02

--- module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-02-chihuahuas-everywhere/main.js
+++ module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-02-chihuahuas-everywhere/main.js
@@ -1,1 +1,26 @@
 'use strict';
+const html = document.querySelector('body');
+html.innerHTML +='<button class="js-dog2">Show me another Chihuahua</button><div><img src="" alt=""><img /></div>';
+
+function getDogImage() {
+    fetch("https://dog.ceo/api/breed/chihuahua/images/random")
+      .then(response => response.json())
+      .then(data => {
+        const img = document.querySelector("img");
+        img.src = data.message;
+        img.alt = "Un perro";
+      });
+  }
+  const btn = document.querySelector(".js-dog");
+  btn.addEventListener("click", getDogImage);
+const btn2 = document.querySelector(".js-dog2");
+btn2.addEventListener("click", getDogImage);
+
+
+
+/*
+    a) Vamos a modificar el ejemplo anterior para que las fotos de nuestra página salgan sólo Chihuahuas.
+    b) Vamos a encapsular toda la lógica para crear una petición en una función. 
+Añadimos un botón a la página con el título 'Enséñame otro Chihuahua' de forma que al pulsarlo se haga 
+otra petición al servidor de una imagen aleatoria y aparezca una nueva imagen de Chihuaua.
+*/
