9ec622efb13e064823247d0884adf943c9458613
cvcastano <carocastanocastillo@gmail.com>
2021-02-02 08:13:09 +0100

    add ex.04 (incorrect)

--- module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-04-repos-list/main.js
+++ module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-04-repos-list/main.js
@@ -1,1 +1,21 @@
 'use strict';
+const button = document.querySelector('button');                   // button selector 
+button.addEventListener("click", findRepos);                        // button event listener
+
+function findRepos() {                                              // define function    
+    const orgName = document.querySelector('input').value;         // input selector                 
+    fetch(`https://api.github.com/orgs/${orgName}`)                 // define fetch with the organization name
+        .then(orgResponse => orgResponse.json())                  // create response
+        .then(orgData => {                                          // receive data from organization 
+            const reposUrl = orgData.repos_url;                     // get the url for the repos               
+            return fetch(reposUrl);
+            console.log(reposUrl)
+        })
+                .then(reposResponse => reposResponse.json())
+                .then(reposData => {
+                    const list = document.querySelector('ul');
+                    for(let i= 0; i< reposData.lenght; i++) {
+                        list.innerHTML += `<li>${reposData[i].name}</li>`
+                    }             
+        });
+}
