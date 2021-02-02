add5a4b47397849be67f7c8401284febbe55179b
cvcastano <carocastanocastillo@gmail.com>
2021-02-02 11:03:06 +0100

    add ex.04

--- module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-04-repos-list/main.js
+++ module 2/module-2-lesson-10-server-requests/module-02-lesson-10-ex-04-repos-list/main.js
@@ -1,21 +1,23 @@
 'use strict';
-const button = document.querySelector('button');                   // button selector 
+const button = document.querySelector('button');                    // button selector 
 button.addEventListener("click", findRepos);                        // button event listener
 
 function findRepos() {                                              // define function    
-    const orgName = document.querySelector('input').value;         // input selector                 
+    const orgName = document.querySelector('input').value;          // input selector                 
     fetch(`https://api.github.com/orgs/${orgName}`)                 // define fetch with the organization name
-        .then(orgResponse => orgResponse.json())                  // create response
+        .then(response1 => response1.json())                        // create response
         .then(orgData => {                                          // receive data from organization 
-            const reposUrl = orgData.repos_url;                     // get the url for the repos               
-            return fetch(reposUrl);
-            console.log(reposUrl)
+            const reposUrl = orgData.repos_url;
+            console.log(reposUrl)                                   // it works!
+            return fetch(reposUrl);                                 // get the url for the repos  
         })
-                .then(reposResponse => reposResponse.json())
-                .then(reposData => {
-                    const list = document.querySelector('ul');
-                    for(let i= 0; i< reposData.lenght; i++) {
-                        list.innerHTML += `<li>${reposData[i].name}</li>`
-                    }             
+        .then(response2 => response2.json())
+        .then(reposData => {
+            console.log(reposData)                                  // it works!
+            const list = document.querySelector('ul');              // list selector
+            for (let i = 0; i < reposData.length; i++) {
+
+                list.innerHTML += `<li>${reposData[i].name}</li>`
+            }
         });
 }
