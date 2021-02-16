dde15412d7b7283b2fc3ba1976e6f4bccb96ad8d
cvcastano <carocastanocastillo@gmail.com>
2021-02-16 18:32:32 +0100

    upload ex. 01

--- module 3/module-03-lesson-01-intro-to-react/src/App.js
+++ module 3/module-03-lesson-01-intro-to-react/src/App.js
@@ -4,14 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Carolina Castaño</h1>
-        <h2>Tuesday, February 16th 2021</h2>
-        <p>
-         
-        </p>
-      </header>
+      <div className="card">
+        <header className="header">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="header-text">
+            <h4 className="name">Carolina Castaño</h4>
+            <h5 className="date">Tuesday, February 16th 2021</h5>
+          </div>
+        </header>
+        <main className="main">
+          <p className="main-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+            repellendus sapiente sint ut officia molestiae incidunt facere
+            magnam minus, aliquid eos animi, soluta sequi harum? Odio non in
+            ducimus aspernatur. Rerum repellendus sapiente sint ut officia molestiae incidunt facere magnam minus, aliquid eos animi, soluta sequi harum? Odio non in ducimus aspernatur...
+          </p>
+        </main>
+        <footer className="footer">
+          <a className="read-more">Read-more...</a>
+          <span className="likes">37 <i className="fas fa-heart"></i></span>
+        </footer>
+      </div>
     </div>
   );
 }
