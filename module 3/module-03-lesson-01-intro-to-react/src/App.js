e535142a539128a963c1ab0350aefccadae0e886
cvcastano <carocastanocastillo@gmail.com>
2021-02-16 20:51:31 +0100

    add ex. 02

--- module 3/module-03-lesson-01-intro-to-react/src/App.js
+++ module 3/module-03-lesson-01-intro-to-react/src/App.js
@@ -1,32 +1,34 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="card">
-        <header className="header">
-          <img src={logo} className="logo" alt="logo" />
-          <div className="header-text">
-            <h4 className="name">Carolina Castaño</h4>
-            <h5 className="date">Tuesday, February 16th 2021</h5>
-          </div>
-        </header>
-        <main className="main">
-          <p className="main-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
-            repellendus sapiente sint ut officia molestiae incidunt facere
-            magnam minus, aliquid eos animi, soluta sequi harum? Odio non in
-            ducimus aspernatur. Rerum repellendus sapiente sint ut officia molestiae incidunt facere magnam minus, aliquid eos animi, soluta sequi harum? Odio non in ducimus aspernatur...
-          </p>
-        </main>
-        <footer className="footer">
-          <a className="read-more">Read-more...</a>
-          <span className="likes">37 <i className="fas fa-heart"></i></span>
-        </footer>
-      </div>
+class App extends Component {
+render() {
+  const header = <header className="header">
+    <img src={logo} className="logo" alt="logo" />
+    <div className="header-text">
+      <h4 className="name">Carolina Castaño</h4>
+      <h5 className="date">Tuesday, February 16th 2021</h5>
     </div>
-  );
+  </header>;
+  const main = <main className="main">
+    <p className="main-text">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+      repellendus sapiente sint ut officia molestiae incidunt facere
+      magnam minus, aliquid eos animi, soluta sequi harum? Odio non in
+      ducimus aspernatur. Rerum repellendus sapiente sint ut officia molestiae incidunt facere magnam minus, aliquid eos animi, soluta sequi harum? Odio non in ducimus aspernatur...
+  </p>
+  </main>;
+  const footer = (<footer className="footer">
+    <a className="read-more">Read-more...</a>
+    <span className="likes">37 <i className="fas fa-heart"></i></span>
+  </footer>);
+  const card = (<div className="card">
+    {header} {main} {footer}
+  </div>);
+  const appRoot = (<div className="App"></div>);
+ return appRoot;
+}
 }
 
 export default App;
