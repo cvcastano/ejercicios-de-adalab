aeb9dbfe9672c753599f2e9be3f9a0cadbe1d4fa
cvcastano <carocastanocastillo@gmail.com>
2021-02-18 20:22:32 +0100

    add ex. 03 folder

--- module 3/module-3-lesson-03-components-in-react/module-3-lesson-03-ex-03/src/App.js
+++ module 3/module-3-lesson-03-components-in-react/module-3-lesson-03-ex-03/src/App.js
@@ -1,35 +1,41 @@
 import React from 'react';
 
-const students = [
-  {
-    promo: 'A',
-    name: 'Sofía',
-    age: 20
-  },
-  {
-    promo: 'B',
-    name: 'María',
-    age: 21
-  },
-  {
-    promo: 'A',
-    name: 'Lucía',
-    age: 22
-  }
-];
-const promoAstudents = students.filter(student => student.promo === 'A');
-
 class App extends React.Component {
   render() {
-   
+    const items = [
+      {
+        name: 'Cereales con chocolate',
+        description: 'Cereales rellenos de chocolate',
+        quantity: 2,
+        category: 'Cereales',
+        price: 5
+      },
+      {
+        name: 'Hamburguesa con queso',
+        description: 'Hamburguesa rica y saludable',
+        quantity: 1,
+        category: 'Fast-food',
+        price: 15
+      },
+      {
+        name: 'Agua mineral',
+        description: 'Agua de un charco del Himalaya',
+        quantity: 2,
+        category: 'Bebida',
+        price: 5
+      }
+    ];
     return (
       <div className="page">
-        <h1>Filtering lists with React:</h1>
-        {promoAstudents.map((promoAstudent, index) => {
-          return (
+        <h1>Pintar listados con React:</h1>
+        {items.map((item, index) => {
+           return (
             <li className="list" key={index}>
-              <h2>Name: {promoAstudent.name}</h2>
-              <p>Age: {promoAstudent.age}</p>
+              <h2>Nombre: {item.name}</h2>
+              <p>Descripción: {item.description}</p>
+              <p>Cantidad: {item.quantity}</p>
+              <p>Categoría: {item.category}</p>
+              <p>Precio: {item.price}</p>
             </li>
           );
         })}
