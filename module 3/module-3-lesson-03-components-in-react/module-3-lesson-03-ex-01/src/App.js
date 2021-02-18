9f10046a5e08b3ecb0a97446851527e31e102154
cvcastano <carocastanocastillo@gmail.com>
2021-02-18 16:12:18 +0100

    add ex. 01

--- module 3/module-3-lesson-03-components-in-react/module-3-lesson-03-ex-01/src/App.js
+++ module 3/module-3-lesson-03-components-in-react/module-3-lesson-03-ex-01/src/App.js
@@ -1,14 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+const students = [
+  {
+    promo: 'A',
+    name: 'Sofía',
+    age: 20
+  },
+  {
+    promo: 'B',
+    name: 'María',
+    age: 21
+  },
+  {
+    promo: 'A',
+    name: 'Lucía',
+    age: 22
+  }
+];
+
+class App extends React.Component {
+  render() {
+   
+    return (
+      <div className="page">
+        <h1>Mapping lists with React:</h1>
+        {students.map((student, index) => {
+          return (
+            <li className="list" key={index}>
+              <h2>Name: {student.name}</h2>
+              <p>Age: {student.age}</p>
+            </li>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
+
