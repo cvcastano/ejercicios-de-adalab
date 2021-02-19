941206086025ff637d05d2033aca5b7216bb5cd6
cvcastano <carocastanocastillo@gmail.com>
2021-02-19 07:00:35 +0100

    add ex.03 folder

---
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/README.md
@@ -0,0 +1,15 @@
+[Exercise 3](https://books.adalab.es/materiales-front-end-l/modulo-3-react/3_4_eventos_react#ejercicio-3)
+
+# Exercise 3
+
+![]()
+
+OnionHater.js
+```javaScript
+
+
+export default OnionHater;
+```
+
+
+
---
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import OnionHater from './OnionHater';
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div className="page">
+        <OnionHater />
+      </div>
+    );
+  }
+}
+
+export default App;
+
