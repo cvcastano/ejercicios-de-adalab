121f8b172f092976ab6c7b06e76d3cdf85f26ef2
cvcastano <carocastanocastillo@gmail.com>
2021-02-19 06:15:10 +0100

    add ex. 01

--- module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-01/README.md
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-01/README.md
@@ -1,22 +1,31 @@
-[Exercise 3](https://books.adalab.es/materiales-front-end-l/modulo-3-react/3_4_eventos_react#ejercicio-1)
+[Exercise 1](https://books.adalab.es/materiales-front-end-l/modulo-3-react/3_4_eventos_react#ejercicio-1)
 
-# Exercise 3
+# Exercise 1
 
 ![]()
 
 App.js
 ```javaScript
+import React from 'react';
 
+const textareaHandler = ev => {
+    const value = ev.target.value;
+    if (value.includes('onion')) {
+        alert ('I hate onions');
+    }
+}
 
+class OnionHater extends React.Component {
+  render() {
+
+    return (
+        <textarea name="" id="" cols="30" rows="10" onKeyUp={textareaHandler}>Write on me:</textarea>
+    );
+  }
+}
+
+export default OnionHater;
 ```
 
-
-```
-
-index.js
-```javaScript
-
-
-```
 
 
--- module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-01/src/App.js
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-01/src/App.js
@@ -1,44 +1,12 @@
 import React from 'react';
+import OnionHater from './OnionHater';
 
 class App extends React.Component {
   render() {
-    const items = [
-      {
-        name: 'Cereales con chocolate',
-        description: 'Cereales rellenos de chocolate',
-        quantity: 2,
-        category: 'Cereales',
-        price: 5
-      },
-      {
-        name: 'Hamburguesa con queso',
-        description: 'Hamburguesa rica y saludable',
-        quantity: 1,
-        category: 'Fast-food',
-        price: 15
-      },
-      {
-        name: 'Agua mineral',
-        description: 'Agua de un charco del Himalaya',
-        quantity: 2,
-        category: 'Bebida',
-        price: 5
-      }
-    ];
+
     return (
       <div className="page">
-        <h1>Pintar listados con React:</h1>
-        {items.map((item, index) => {
-           return (
-            <li className="list" key={index}>
-              <h2>Nombre: {item.name}</h2>
-              <p>Descripción: {item.description}</p>
-              <p>Cantidad: {item.quantity}</p>
-              <p>Categoría: {item.category}</p>
-              <p>Precio: {item.price}</p>
-            </li>
-          );
-        })}
+        <OnionHater />
       </div>
     );
   }
