01f3b36d2854fbab31e4666650a8c3f1a1d0dde6
cvcastano <carocastanocastillo@gmail.com>
2021-02-19 08:10:35 +0100

    add ex.03

--- module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/README.md
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/README.md
@@ -6,7 +6,40 @@
 
 OnionHater.js
 ```javaScript
+import React from 'react';
 
+const page = document.querySelector('.page');
+
+class OnionHater extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+    this.isHating = false
+  }
+
+  handleInput(ev){
+    const value = ev.target.value.toLowerCase();
+    
+    // console.log(value);
+
+    if (value.includes('onion')) {
+      this.isHating = true;
+      page.classList.add('hatred');
+    }
+    else {
+      this.isHating = false;
+      page.classList.remove('hatred');
+    }
+  }
+  
+  render() {
+
+    return (
+      <textarea cols="30" rows="10" className="textarea" onKeyUp={this.handleInput}></textarea>
+    );
+  }
+}
 
 export default OnionHater;
 ```
--- module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/src/OnionHater.js
+++ module 3/module-3-lesson-04-events-in-react/module-3-lesson-04-ex-03-my-hatred-in-red/src/OnionHater.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
-const textareaHandler = ev => {
-    const value = ev.target.value;
-    if (value.includes('onion')) {
-        alert ('I hate onions');
-    }
-}
+
+const page = document.querySelector('.page');
 
 class OnionHater extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleInput = this.handleInput.bind(this);
+    this.isHating = false
+  }
+
+  handleInput(ev){
+    const value = ev.target.value.toLowerCase();
+    
+    // console.log(value);
+
+    if (value.includes('onion')) {
+      this.isHating = true;
+      page.classList.add('hatred');
+    }
+    else {
+      this.isHating = false;
+      page.classList.remove('hatred');
+    }
+
+  }
   render() {
 
     return (
-        <textarea name="" id="" cols="30" rows="10" onKeyUp={textareaHandler}>Write on me:</textarea>
+      <textarea cols="30" rows="10" className="textarea" onKeyUp={this.handleInput}></textarea>
     );
   }
 }
