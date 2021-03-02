3853201d552238f4d9b55c04f8404943ad14cd40
cvcastano <carocastanocastillo@gmail.com>
2021-03-02 11:22:39 +0100

    add exercises

---
+++ module 3/module-3-lesson-10-components-lifecycle/module-3-lesson-10-ex-01-lifecycle-counter/src/components/Relax.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../stylesheets/index.scss';
+
+class Relax extends React.Component {
+
+    render() {
+        return <div>I will rest my mind and my eyes soon</div>;
+    }
+}
+
+export default Relax;
