bedb7ec56255658cdc7eed6ca15bc0f6ccc56294
cvcastano <you@example.com>
2021-04-11 16:20:45 +0200

    Correct ex 09

--- module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-09-number-of-letters-in-your-name/main.js
+++ module 2/module-2-lesson-02-intro-to-programming-2/module-02-lesson-02-ex-09-number-of-letters-in-your-name/main.js
@@ -3,4 +3,7 @@ const name = 'Carolina';
 const surname = 'Castaño'; 
 const surname2 = 'Castillo';
 const charactersSum = name + surname + surname2;
-alert (`Mi nombre es ${name} ${surname} ${surname2}, y está compuesto por ${charactersSum.length} caracteres`);
+
+const title = document.querySelector('h1');
+
+title.innerHTML = `Mi nombre es ${name} ${surname} ${surname2}, y está compuesto por ${charactersSum.length} caracteres`;
