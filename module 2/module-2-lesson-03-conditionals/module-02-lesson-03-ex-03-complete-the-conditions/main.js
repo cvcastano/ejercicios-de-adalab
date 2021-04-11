6e16ac2f4aed14492594c588637041fec2cf77e2
cvcastano <you@example.com>
2021-04-11 18:47:04 +0200

    Correct ex.03

--- module 2/module-2-lesson-03-conditionals/module-02-lesson-03-ex-03-complete-the-conditions/main.js
+++ module 2/module-2-lesson-03-conditionals/module-02-lesson-03-ex-03-complete-the-conditions/main.js
@@ -2,9 +2,9 @@
 const num = 6;
 if (num === 0) {
     console.log('El número es 0')
-  } else if (num <0) {
+  } else if (num < 0) {
     console.log('El número es negativo')
-  } else if (num +2 > 13 && num <= 20) {
+  } else if (num + 2 > 13 && num + 2 <= 20) {
     console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
   } else if (num > 20 && num < 50) {
     console.log('El número es mayor que 20 pero menor que 50')
