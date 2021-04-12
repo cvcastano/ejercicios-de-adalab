3019d7bbda7457b6ee2d5d845290fbe7fb2469e0
cvcastano <you@example.com>
2021-04-12 21:44:17 +0200

    Comment ex. 03

--- module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-03-ticket-with-vat/main.js
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-03-ticket-with-vat/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
+// Crea una función que reciba por parámetro un número, que representará un precio, y devuelva un texto en el que ponga el precio sin IVA, el IVA (21%) y el total. Por ejemplo, si pasamos por parámetro un 10, la función pintará en la consola "Precio sin IVA: 10, IVA: 2,1 y Total: 12,1".
+// Para probar que funciona, ejecuta la función recogiendo el resultado en una variable e imprímela en la consola para comprobarlo.
+
 function iva(a) {
-    return `Precio sin IVA: ${a}, IVA: ${a*0.21} y Total: ${a+ a*0.21}`; 
+    return `Precio sin IVA: ${a}, IVA: ${a * 0.21} y Total: ${a + a * 0.21}`;
 }
-iva(10);
 
-console.log (iva(10));
-    
-// "Precio sin IVA: 10, IVA: 2,1 y Total: 12,1"
+console.log(iva(10));
 
-    
-   
+
+
+
