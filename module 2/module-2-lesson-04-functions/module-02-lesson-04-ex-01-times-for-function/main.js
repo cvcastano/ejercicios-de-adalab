2e139874836ee700c32baa1fc773b4458c93f7f0
cvcastano <you@example.com>
2021-04-12 21:37:21 +0200

    Comment ex. 01

--- module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-01-times-for-function/main.js
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-01-times-for-function/main.js
@@ -1,11 +1,12 @@
 'use strict';
+// Crea una función que reciba como argumentos dos valores y devuelva como valor de retorno la multiplicación de ambos. Haz tres pruebas con distintos números para comprobar que funciona correctamente y muestra el resultado en la consola usando console.log().
 
 function timesFor(a, b) {
     const result = a * b;
-    alert (result);
+    alert(result);
 }
 // const timesForResult = timesFor(7, 3);
 // const timesForResult = timesFor(9, 6);
 const timesForResult = timesFor(5, 8);
 
-
+// * Yo he usado alert en lugar de console.log, por que me mola más. 
