71918ecb5cfdba885205fa7e25ebdd53cbb12e51
cvcastano <you@example.com>
2021-04-12 10:40:19 +0200

    Add ex. 06

--- module 2/module-2-lesson-03-conditionals/module-02-lesson-03-ex-06-ternary-operator/main.js
+++ module 2/module-2-lesson-03-conditionals/module-02-lesson-03-ex-06-ternary-operator/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// let avocados;
+const avocadoPrice = 1.5;
+const money = 33;
+// if (money >= avocadoPrice) {
+//   avocados = money / avocadoPrice;
+// } else {
+//   avocados = 0;
+// }
+// console.log (avocados);
+
+
+// Reescribe el código del anterior utilizando un ternario en lugar de un if...else.
+
+const avocados = money >= avocadoPrice ? money / avocadoPrice : 0;
+
+console.log(avocados)
