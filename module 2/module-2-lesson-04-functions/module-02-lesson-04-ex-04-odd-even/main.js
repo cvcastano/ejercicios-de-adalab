35cd105eac1768daedb4bbff41b6df896c8e0cc9
cvcastano <you@example.com>
2021-04-12 21:53:03 +0200

    Comment ex. 04

--- module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-04-odd-even/main.js
+++ module 2/module-2-lesson-04-functions/module-02-lesson-04-ex-04-odd-even/main.js
@@ -1,15 +1,18 @@
 'use strict';
+//  Crear una función que reciba por parámetro un número y devuelva true si es par y false si es impar.
+//  Ejecútala e imprime el resultado para comprobar que funciona.
+
 function oddEven(num) {
-if (num % 2 === 0) {
-    return true;
+    if (num % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-else {
-    return false;
-}
-}
-alert (oddEven(8));
-alert (oddEven(9));
-alert (oddEven (456));
+console.log(oddEven(8));
+console.log(oddEven(9));
+console.log(oddEven(456));
 
 
-// slash (/) divide, el porciento (%) halla el resto de la división
+// slash (/) divide; el porciento (%) halla el resto de la división
