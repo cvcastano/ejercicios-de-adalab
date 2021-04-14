9969d7be4edf840e046e963fc34fd140f33e192f
cvcastano <you@example.com>
2021-04-14 11:39:10 +0200

    Comment ex. 02

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-02-what-is-your-name/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-02-what-is-your-name/main.js
@@ -1,4 +1,10 @@
 'use strict';
+// Crear una página HTML con un input de tipo texto para introducir tu nombre y un botón. 
+// Al pinchar sobre el botón, imprimir en la consola el mensaje 'Hola {nombre}', con el nombre que aparece en el input de texto.
+// Nota: La etiqueta input no tiene apertura y cierre, por lo tanto técnicamente no tiene contenido. 
+// Si para leer y modificar el contenido de una etiqueta con apertura y cierre utilizábamos innerHTML, 
+// en el caso de los inputs utilizaremos value.
+
 // coger los elementos de HTML
 const button = document.querySelector('button');
 const text = document.querySelector('input');
