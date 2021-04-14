693818a4e1c6a651ebd4c7b8a27030d250a9e9b6
cvcastano <you@example.com>
2021-04-14 11:28:43 +0200

    Comment ex. 01

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-01-hello-click/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-01-hello-click/main.js
@@ -1,4 +1,7 @@
 'use strict';
+// Crear una página HTML con un párrafo en el que ponga Hola y un botón. 
+// Cuando se pulse el botón hay que cambiar ese texto por "Mi primer click, ¡ole yo y la mujer que me parió!
+
 // elementos de HTML
 const button = document.querySelector ('button');
 const title = document.querySelector ('h1');
