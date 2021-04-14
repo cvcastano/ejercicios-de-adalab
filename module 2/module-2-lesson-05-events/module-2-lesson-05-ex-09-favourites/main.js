c0f6805723d34098d45fbb8f938b5fb7136a6ecb
cvcastano <you@example.com>
2021-04-14 15:55:43 +0200

    Finish exercise 09

--- module 2/module-2-lesson-05-events/module-2-lesson-05-ex-09-favourites/main.js
+++ module 2/module-2-lesson-05-events/module-2-lesson-05-ex-09-favourites/main.js
@@ -2,38 +2,35 @@
 const isra = document.querySelector('.teacher--isra');
 const tuerto = document.querySelector('.teacher--tuerto');
 const nasi = document.querySelector('.teacher--nasi');
-const container = document.querySelector('ul');
+const container = document.querySelector('.teachers');
+// const favorite = document.querySelectorAll('.favorite');
 
 
-function addClass() {
-
+function addClass(event) {
    const chosenTeacher = event.currentTarget;
    chosenTeacher.classList.toggle('teacher--selected');
-
+   const clicked = chosenTeacher.querySelector('.favorite');
+   if (clicked.innerHTML === 'Añadir') {
+      clicked.innerHTML = 'Quitar'
+   } else if (clicked.innerHTML === 'Quitar') {
+      clicked.innerHTML = 'Añadir'
+   }
 }
+
 isra.addEventListener('click', addClass);
 tuerto.addEventListener('click', addClass);
 nasi.addEventListener('click', addClass);
 
 
-
-
-
-
-
-
 // Al pinchar en un elemento del listado tenemos que:
-// Añadir la clase .teacher--selected si no la tiene, 
-// o quitarla si la tiene.
-// Modificar el texto del span .favorite sustituyéndolo 
-// por 'Quitar' si en ese momento contiene 'Añadir', 
-// o por 'Añadir' si contiene 'Quitar'.
+// Añadir la clase .teacher--selected si no la tiene, o quitarla si la tiene.
+// Modificar el texto del span .favorite sustituyéndolo por:
+// 'Quitar' si en ese momento contiene 'Añadir' o
+// 'Añadir' si contiene 'Quitar'.
 
-// Nota: con querySelector buscamos un elemento dentro 
-// de otro. Hasta ahora lo habíamos usado para buscar 
-// dentro de document (todo nuestro documento HTML), 
-// con document.querySelector().
-// Si tuviéramos una constante llamada, por ejemplo, 
-// sectionAboutElement en la que hemos guardado un 
-// elemento de HTML, podríamos buscar dentro él otro 
-// elemento, tal que así sectionAboutElement.querySelector().
+// Nota: con querySelector buscamos un elemento dentro de otro. 
+// Hasta ahora lo habíamos usado para buscar dentro de document 
+// (todo nuestro documento HTML), con document.querySelector().
+// Si tuviéramos una constante llamada, por ejemplo, sectionAboutElement 
+// en la que hemos guardado un elemento de HTML, podríamos buscar dentro de ella 
+// otro elemento, tal que así sectionAboutElement.querySelector().
