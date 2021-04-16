a0aead63425ad81fec5c275eaa63d6f56869059b
cvcastano <you@example.com>
2021-04-16 22:07:52 +0200

    Add ex. 07

--- module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-07-user-data-structure/main.js
+++ module 2/module-2-lesson-06-objects/module-2-lesson-06-ex-07-user-data-structure/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const userData = {};
+
+const job = 'developer';
+
+userData.firstName = 'Pepita';
+userData.lastName = 'Castaño';
+userData['age'] = 42;
+userData.job = job;
+
+console.log(userData.firstName);
+console.log(userData['age'] + 1);
+console.log(userData['job']);
+
