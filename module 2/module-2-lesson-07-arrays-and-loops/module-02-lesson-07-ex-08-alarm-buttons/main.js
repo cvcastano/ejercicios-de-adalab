71d90e8aae56bd50f4159be06f1f0bb033eb9851
cvcastano <you@example.com>
2021-04-18 20:05:24 +0200

    Add ex. 08

--- module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-08-alarm-buttons/main.js
+++ module 2/module-2-lesson-07-arrays-and-loops/module-02-lesson-07-ex-08-alarm-buttons/main.js
@@ -1,1 +1,16 @@
 'use strict';
+// Vamos a partir de un HTML que tiene 3 botones con el texto ALARMA en un fondo blanco. 
+// Vamos a hacer que al pulsar en cualquiera de ellos, el fondo de la pantalla se ponga rojo. 
+// Si volvemos a pulsar en cualquiera de ellos, el fondo se pondrá blanco. Y así sucesivamente. 
+// Vamos a hacer uso de querySelectorAll para evitar repetir mucho código.
+
+const buttons = document.querySelectorAll('button');
+const page = document.querySelector('body');
+
+function handleClick() {
+    page.classList.toggle('red');
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', handleClick);
+}
