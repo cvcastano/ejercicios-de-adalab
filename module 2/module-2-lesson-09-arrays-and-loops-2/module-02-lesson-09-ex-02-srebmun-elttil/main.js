71a364e96d38a0fe83919dc5b2364a35d42367e7
cvcastano <you@example.com>
2021-04-19 15:34:17 +0200

    Comment and indent

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
@@ -2,15 +2,20 @@
 
 let array100 = []
 
- function get100Numbers () {
-    for (let i = 0; i < 100; i++) {
-       array100.push(i + 1); 
-    }
-return array100
+function get100Numbers() {
+   for (let i = 0; i < 100; i++) {
+      array100.push(i + 1);
+   }
+   return array100
 }
- 
-function getReversed100Numbers () {
+
+function getReversed100Numbers() {
    return console.log(get100Numbers().reverse());
 }
 
 getReversed100Numbers();
+
+
+/*
+Vamos a crear una nueva función getReversed100Numbers que llama a la función del ejercicio anterior para obtener 100 números y los cambia de orden. Para comprobar que los tenemos todos, vamos a ejecutar la función y a loguearlos (con console.log) uno a uno en la consola en orden.
+*/
