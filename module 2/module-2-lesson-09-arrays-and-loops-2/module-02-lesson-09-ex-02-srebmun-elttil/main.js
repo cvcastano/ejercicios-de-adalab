7defb79d764288d7ce5e26da21a21f72874304b3
cvcastano <you@example.com>
2021-04-19 15:32:58 +0200

    Redo ex. 02

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-02-srebmun-elttil/main.js
@@ -3,15 +3,14 @@
 let array100 = []
 
  function get100Numbers () {
-    for (let i= 0; i<100; i++) {
-       array100.push(i+1);
-       
+    for (let i = 0; i < 100; i++) {
+       array100.push(i + 1); 
     }
 return array100
 }
  
 function getReversed100Numbers () {
-   let reversedArray100 = get100Numbers().reverse();
-    console.log(reversedArray100);
+   return console.log(get100Numbers().reverse());
 }
+
 getReversed100Numbers();
