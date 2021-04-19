e417f52b2dc7975b4e4cacda85e82ab54c0dc82e
cvcastano <you@example.com>
2021-04-19 23:46:53 +0200

    Add ex. 05

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-05-create-your-x-mas-tree/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-05-create-your-x-mas-tree/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function drawTree(height) {
+    let tree = "";
+    let triangle = '▲';
+    for (let i = 0; i < height; i++) {
+        tree += triangle
+        console.log(tree)
+    }
+}
+drawTree(5)
+
