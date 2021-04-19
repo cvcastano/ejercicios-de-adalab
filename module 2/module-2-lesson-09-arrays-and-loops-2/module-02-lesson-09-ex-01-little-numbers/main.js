8570f7c4b86ec40e5f636a1d6cc985e756228a7b
cvcastano <you@example.com>
2021-04-19 15:14:06 +0200

    Change ex. 01

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-01-little-numbers/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-01-little-numbers/main.js
@@ -3,16 +3,16 @@
  let array100 = []
 
  function get100Numbers () {
-    for (let i= 0; i<100; i++) {
-       array100.push(i+1);
-       console.log(array100);
+    for (let i= 0; i < 100; i++) {
+       array100.push(i + 1);
     }
-
+    return console.log(array100);
 }
 get100Numbers();
+
 /*
 Vamos a crear una función get100Numbers que devuelve un array con los números del 1 al 100. 
 Como no nos apetece tener que escribir 100 números a mano, usaremos un bucle y el método push para ir 
-guardándolos. Para comprobar que los tenemos todos, vamos a ejecutar la función y loguearlos 
-(con console.log) uno a uno en la consola en orden.
-*/
+guardándolos. Para comprobar que los tenemos todos, vamos a ejecutar la función y loguearlos con console.log.
+*/  
+
