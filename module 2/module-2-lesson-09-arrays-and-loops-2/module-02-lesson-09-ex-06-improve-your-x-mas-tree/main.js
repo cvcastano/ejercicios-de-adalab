ee41cbba5a4b635a6206cd507776dd9d78e05972
cvcastano <you@example.com>
2021-04-20 00:39:45 +0200

    Add ex. 06

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-06-improve-your-x-mas-tree/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-06-improve-your-x-mas-tree/main.js
@@ -1,1 +1,18 @@
 'use strict';
+function drawTree(height) {
+
+    let triangle = '▲';
+    let firstRound = true;
+    let tree = "";
+    for (let i = 0; i < height; i++) {
+        if (firstRound === true) {
+            console.log('★');
+            firstRound = false;
+        } else {
+            tree += triangle;
+            console.log(tree);
+        }
+    }
+    console.log('|')
+}
+drawTree(5);
