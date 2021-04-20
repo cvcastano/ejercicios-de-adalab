eb990c987351569e7c74e57da85ae3d458570e15
cvcastano <you@example.com>
2021-04-20 10:37:15 +0200

    Add ex. 07

--- module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-07-that-is-a-spruce/main.js
+++ module 2/module-2-lesson-09-arrays-and-loops-2/module-02-lesson-09-ex-07-that-is-a-spruce/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// Intenta cambiar el código para que aparezca el árbol completo.
+
+console.log('        🌟');
+let triangle = '🎄';
+let blank = '  ';
+
+function drawTree() {
+    for (let nTriangles = 1, nBlanks = 4; nTriangles <= 9; nTriangles += 2, nBlanks--) {
+
+        console.log(blank.repeat(nBlanks) + triangle.repeat(nTriangles));
+    }
+    console.log('        🟫');
+}
+drawTree()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function drawTree() {
+//     console.log('        🌟');
+//     let triangle = '🎄';
+//     let space = '  ';
+
+//     for (let numTriangles = 1, numSpaces = 4; numTriangles <= 9; numTriangles += 2, numSpaces--) {
+//         let spaces = space.repeat(numSpaces);
+//         let triangles = triangle.repeat(numTriangles);
+//         console.log(spaces + triangles)
+
+//     }
+//     console.log('        🟫');
+// }
+// drawTree();
+
+
