123649b0b254a25405d75fe73d9e82edda162d33
cvcastano <you@example.com>
2021-05-25 10:30:14 +0200

    Add ex. 02

--- module 3/module-3-lesson-11-testing-with-javascript/src/App.test.js
+++ module 3/module-3-lesson-11-testing-with-javascript/src/App.test.js
@@ -14,6 +14,15 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+// Exercise 2
+test('check if "Learn React" is a link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Aprendiendo react/i);
+  console.log(linkElement.nodeName);
+  const linkTagName = linkElement.nodeName;
+  expect(linkTagName).toBe("A");
+})
+
 /*
 Exercise 1
 Supongamos que ahora nos viene la diseñadora y nos pide que en nuestra página no ponga Learn React, sino que ponga Aprendiendo React.
@@ -23,4 +32,9 @@ Arranca los tests en otra terminal con npm run test.
 Verás que el test está fallando. Hemos roto los tests, porque nuestra aplicación ha cambiado. Cada vez que hacemos un cambio en la aplicación debemos volver a ejecutar los tests para ver si fallan y si fallan hay que arreglarlos. ¿Qué debes cambiar en App.test.js para que el test vuelva a funcionar.
 Nota: normalmente hacemos testing con dos terminales abiertas, una para tener el proyecto arrancado y otra para tener los tests corriendo. Y siempre miramos la terminal para estar atentas a ver si los tests se rompen.
 Nota: los errores que aparecen en la terminal nos dan mucha información. Lo lógico es leer detenidamente dicha información pasar qué está fallando, en qué línea, en qué fichero...
+
+Exercise 2
+Testear el tipo de la etiqueta
+En el ejercicio 1 hemos visto que el test comprueba si hay una etiqueta HTML que tiene el texto Aprendiendo React.
+Ahora vamos a testear de qué tipo es esa etiqueta HTML, si es un link, un párrafo, una cabecera...
 */
