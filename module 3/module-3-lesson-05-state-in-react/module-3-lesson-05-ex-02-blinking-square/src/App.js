b3b02c1b4558475b3522d0c5014dc94f763ad4d2
cvcastano <carocastanocastillo@gmail.com>
2021-06-06 13:15:05 +0200

    Add ex. 02

--- module 3/module-3-lesson-05-state-in-react/module-3-lesson-05-ex-02-blinking-square/src/App.js
+++ module 3/module-3-lesson-05-state-in-react/module-3-lesson-05-ex-02-blinking-square/src/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'blue'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick() {
+    this.setState((state) => {
+      let nextColor;
+      if (state.color === 'blue') {
+        nextColor = 'red';
+      } else {
+        nextColor = 'blue';
+      }
+      return {
+        color: nextColor
+      }
+    })
+  }
 
   render() {
     return (
-      <>
+      <div className={`square ${this.state.color}`} onClick={this.handleClick}>
 
-      </>
+      </div>
     );
   }
 }
-
 export default App;
