ca5ea79ffda62c84c8b29e0051eddc839e1b7bf2
cvcastano <carocastanocastillo@gmail.com>
2021-06-25 11:25:44 +0200

    Render list

--- module 3/module-3-lesson-05-state-in-react/module-3-lesson-05-ex-07-fresh-fruits/src/App.js
+++ module 3/module-3-lesson-05-state-in-react/module-3-lesson-05-ex-07-fresh-fruits/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      popularFruits: ['kiwi', 'pinneaple', 'strawberry'],
+      newFruit: ''
+    };
+  }
 
   render() {
+    const renderArray = this.state.popularFruits.map((fruit) => {
+      return (
+        <li>
+          {fruit}
+        </li>
+      )
+    });
+
     return (
       <>
-
+        <ul>
+          {renderArray}
+        </ul>
       </>
     );
   }
